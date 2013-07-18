e594118b77e50e1a5864806679e3dc3e901fe532
sivartydrup <sivartydrup@gmail.com>
2013-07-18 21:08:56 +1000

    Added project

---
+++ FormUDPServer.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace UDPServer
+{
+    partial class FormUDPServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxScreenDisplay = new System.Windows.Forms.PictureBox();
+            this.timerReceiveAndDisplay = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreenDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxScreenDisplay
+            // 
+            this.pictureBoxScreenDisplay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxScreenDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxScreenDisplay.Name = "pictureBoxScreenDisplay";
+            this.pictureBoxScreenDisplay.Size = new System.Drawing.Size(658, 282);
+            this.pictureBoxScreenDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxScreenDisplay.TabIndex = 0;
+            this.pictureBoxScreenDisplay.TabStop = false;
+            // 
+            // timerReceiveAndDisplay
+            // 
+            this.timerReceiveAndDisplay.Interval = 10;
+            this.timerReceiveAndDisplay.Tick += new System.EventHandler(this.timerReceiveAndDisplay_Tick);
+            // 
+            // FormUDPServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 306);
+            this.Controls.Add(this.pictureBoxScreenDisplay);
+            this.Name = "FormUDPServer";
+            this.Text = "UDP Server";
+            this.Load += new System.EventHandler(this.FormUDPServer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreenDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxScreenDisplay;
+        private System.Windows.Forms.Timer timerReceiveAndDisplay;
+
+    }
+}
+
---
+++ FormUDPServer.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2013 Travis Purdy. All Rights Reserved.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace UDPServer
+{
+    public partial class FormUDPServer : Form
+    {
+        UdpClient ReceiveClient;
+        IPEndPoint ReceiveEndPoint;
+
+        Image ReceiveImage;
+
+        public FormUDPServer()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUDPServer_Load(object sender, EventArgs e)
+        {
+            ReceiveClient = new UdpClient(11000);
+            ReceiveEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+            timerReceiveAndDisplay.Enabled = true;
+        }
+
+        private void timerReceiveAndDisplay_Tick(object sender, EventArgs e)
+        {
+            if (ReceiveImage != null)
+            {
+                ReceiveImage.Dispose();
+            }
+            MemoryStream ms = new MemoryStream(ReceiveClient.Receive(ref ReceiveEndPoint));
+            ReceiveImage = Image.FromStream(ms);
+            pictureBoxScreenDisplay.Image = ReceiveImage;
+            ms.Close();
+            ms.Dispose();
+            Application.DoEvents();
+        }
+
+    }
+}
